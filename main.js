81a893bc6bfcab461d09bd85fa877da46439a7c1
lamoro8800 <140354637+lamoro8800@users.noreply.github.com>
2023-09-20 15:21:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 1700,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(".text", { delay: 200, origin: "top" });
+sr.reveal(".form-container form", { delay: 400, origin: "left" });
+sr.reveal(".heading", { delay: 100, origin: "top" });
+sr.reveal(".ride-container .box", { delay: 100, origin: "top" });
+sr.reveal(".services-container .box", { delay: 250, origin: "top" });
+sr.reveal(".about-container", { delay: 250, origin: "top" });
+sr.reveal(".reviews-container .box", { delay: 250, origin: "top" });
